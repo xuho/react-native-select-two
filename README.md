407f504fc97b3b766d971214a9169fd185ed8112
Nguyễn Xuân Hoà <hoanx.neonstudio@gmail.com>
2019-08-26 11:21:35 +0700

    Add readme, add single select mode, change some UI design

---
+++ README.md
@@ -0,0 +1,77 @@
+
+## react-native-select-two
+## Component like [Select2](https://select2.org/) on web for React Native
+
+
+## Add it to your project
+
+1. Insall package
+    - Using NPM
+    `npm install react-native-select-two` 
+    - Using Yarn
+    `yarn add react-native-select-two`
+2. Import package
+    `import Select2 from 'react-native-select-two';`
+
+## Usage
+
+
+
+```javascript
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Select2 from 'react-native-select-two';
+
+const mockData = [
+    { id: 1, name: 'React Native Developer' },
+    { id: 2, name: 'Android Developer' },
+    { id: 3, name: 'iOS Developer' }
+];
+
+// create a component
+class CreateNewAppointment extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Select2
+                    isSelectSingle
+                    style={{ borderRadius: 5 }}
+                    colorTheme={'blue'}
+                    popupTitle='Select item'
+                    title='Select item'
+                    data={mockData}
+                    onSelect={data => {
+                        this.setState({ data });
+                    }}
+                    onRemoveItem={data => {
+                        this.setState({ data });
+                    }} 
+                />
+            </View>
+        );
+    }
+}
+```
+
+## Usage
+
+``Single select``
+![Single select](https://raw.githubusercontent.com/xuho/demo-images/master/react-native-select2-single-select.gif)
+``Multiple select``
+![Multiple select](https://raw.githubusercontent.com/xuho/demo-images/master/react-native-select2-multipe-select.gif)
+
+
+
+## Props
+
+- **`style`** _(Object)_ - Custom style for component
+- **`title`** _(String)_ String display when you don't select any item
+- **`onSelect`** _(Function)_ - The callback function trigger after you press select button
+- **`onRemoveItem`** _(String)_ - The callback function trigger after you press tags to remove them
+- **`popupTitle`** _(String)_ - Title of modal select item
+- **`colorTheme`** _(String)_ - Color theme
+- **`isSelectSingle`** _(Bool)_ - Set ```false``` if you want select multiple
+- **`cancelButtonText`** _(String)_ - Cancel button text title
+- **`selectButtonText`** _(String)_ - Select button text title
+
+**MIT Licensed**
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 //import liraries
 import React, { Component } from 'react';
-import { Text, StyleSheet, TouchableOpacity, View, FlatList, TextInput, Dimensions } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity, View, FlatList, TextInput, Dimensions, Animated } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Modal from 'react-native-modal';
 import Button from './lib/Button';
 import TagItem from './lib/TagItem';
 import utilities from './lib/utilities';
+import PropTypes from 'prop-types';
 
 const { height } = Dimensions.get('window');
 // create a component
 class Select2 extends Component {
+    static defaultProps = {
+        cancelButtonText: 'Hủy',
+        selectButtonText: 'Chọn',
+        listEmptyTitle: 'Không tìm thấy lựa chọn phù hợp',
+        colorTheme: '#16a45f'
+    }
     state = {
         show: false,
         preSelectedItem: [],
@@ -17,6 +24,7 @@ class Select2 extends Component {
         data: [],
         keyword: ''
     }
+    animatedHeight = new Animated.Value(height * 0.6);
     componentDidMount() {
         let preSelectedItem = [];
         let { data } = this.props;
@@ -52,85 +60,117 @@ class Select2 extends Component {
         this.setState({ data, show: false, keyword: '', selectedItem: preSelectedItem });
     }
 
-    render() {
-        let { style, title, onSelect, onRemoveItem, popupTitle } = this.props;
-        let { show, data, selectedItem, preSelectedItem } = this.state;
+    onItemSelected = (item, isSelectSingle) => {
+        let selectedItem = [];
+        let { data } = this.state;
+        item.checked = !item.checked;
+        for (let index in data) {
+            if (data[index].id === item.id) {
+                data[index] = item;
+            } else if (isSelectSingle) {
+                data[index].checked = false;
+            }
+        }
+        data.map(item => {
+            if (item.checked) selectedItem.push(item);
+        })
+        this.setState({ data, selectedItem });
+    }
+    keyExtractor = (item, idx) => item.id ? item.id.toString() : idx.toString();
+    renderItem = ({ item, idx }) => {
+        let { colorTheme = '#16a45f', isSelectSingle } = this.props;
         return (
             <TouchableOpacity
-                onPress={() => {
-                    this.setState({ show: true });
-                }}
+                key={item.id ? item.id.toString() : idx.toString()}
+                onPress={() => this.onItemSelected(item, isSelectSingle)}
+                activeOpacity={0.7}
+                style={styles.itemWrapper}>
+                <Text style={styles.itemText}>
+                    {item.name}
+                </Text>
+                <Icon style={styles.itemIcon}
+                    name={item.checked ? 'check-circle-outline' : 'radiobox-blank'}
+                    color={item.checked ? colorTheme : '#777777'} size={20} />
+            </TouchableOpacity>
+        );
+    }
+    renderEmpty = () => {
+        let { listEmptyTitle } = this.props;
+        return (
+            <Text style={styles.empty}>
+                {listEmptyTitle}
+            </Text>
+        );
+    }
+    closeModal = () => this.setState({ show: false });
+    showModal = () => this.setState({ show: true });
+
+    render() {
+        let {
+            style, title, onSelect, onRemoveItem, popupTitle, colorTheme,
+            isSelectSingle, cancelButtonText, selectButtonText
+        } = this.props;
+        let { show, selectedItem, preSelectedItem } = this.state;
+        return (
+            <TouchableOpacity
+                onPress={this.showModal}
                 activeOpacity={0.7}
                 style={[styles.container, style]}>
                 <Modal
+                    onBackdropPress={this.closeModal}
+                    style={{
+                        justifyContent: 'flex-end',
+                        margin: 0
+                    }}
                     useNativeDriver={true}
                     animationInTiming={300}
                     animationOutTiming={300}
                     hideModalContentWhileAnimating
                     isVisible={show}>
-                    <View style={styles.modalContainer}>
+                    <Animated.View style={[styles.modalContainer, { height: this.animatedHeight }]}>
                         <View>
-                            <Text style={styles.title}>
+                            <Text style={[styles.title, { color: colorTheme }]}>
                                 {popupTitle || title}
                             </Text>
                         </View>
                         <View style={styles.line} />
                         <TextInput
                             underlineColorAndroid='transparent'
-                            style={{
-                                height: 40, borderRadius: 2, borderWidth: 1, borderColor: '#cacaca',
-                                paddingLeft: 8, marginVertical: 10, marginHorizontal: 24
-                            }}
-                            selectionColor={'#16a45f'}
+                            returnKeyType='done'
+                            style={styles.inputKeyword}
+                            placeholder='Nhập từ khóa'
+                            selectionColor={colorTheme}
                             onChangeText={keyword => this.setState({ keyword })}
+                            onFocus={() => {
+                                Animated.spring(this.animatedHeight, {
+                                    toValue: height * 0.6 + height * 0.2,
+                                    friction: 7
+                                }).start();
+                            }}
+                            onBlur={() => {
+                                Animated.spring(this.animatedHeight, {
+                                    toValue: height * 0.6,
+                                    friction: 7
+                                }).start();
+                            }}
                         />
                         <FlatList
-                            style={{ paddingHorizontal: 24 }}
+                            style={styles.listOption}
                             data={this.dataRender || []}
-                            keyExtractor={(item, index) => index.toString()}
-                            renderItem={({ item, index }) => {
-                                return (
-                                    <TouchableOpacity
-                                        onPress={() => {
-                                            let selectedItem = [];
-                                            let { data } = this.state;
-                                            item.checked = !item.checked;
-                                            for (let index in data) {
-                                                if (data[index].id === item.id) {
-                                                    data[index] = item;
-                                                    break;
-                                                }
-                                            }
-                                            data.map(item => {
-                                                if (item.checked) selectedItem.push(item);
-                                            })
-                                            this.setState({ data, selectedItem });
-                                        }}
-                                        activeOpacity={0.7}
-                                        style={{
-                                            borderBottomWidth: 1, borderBottomColor: '#eaeaea',
-                                            paddingVertical: 12, flexDirection: 'row', alignItems: 'center'
-                                        }}>
-                                        <Text style={{ fontSize: 16, color: '#333', flex: 1 }}>
-                                            {item.name}
-                                        </Text>
-                                        <Icon style={{ width: 30, textAlign: 'right' }}
-                                            name={item.checked ? 'check-circle-outline' : 'radiobox-blank'}
-                                            color={item.checked ? '#16a45f' : '#777777'} size={20} />
-                                    </TouchableOpacity>
-                                );
-                            }}
+                            keyExtractor={this.keyExtractor}
+                            renderItem={this.renderItem}
+                            ListEmptyComponent={this.renderEmpty}
                         />
 
-                        <View style={{ marginVertical: 16, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', }}>
+                        <View style={styles.buttonWrapper}>
                             <Button
                                 onPress={() => {
                                     this.cancelSelection();
                                 }}
-                                title='Hủy'
-                                textColor={'#16a45f'}
+                                title={cancelButtonText}
+                                textColor={colorTheme}
                                 backgroundColor='#fff'
-                                style={{ height: 36, width: 120, marginRight: 5, marginLeft: 10, borderWidth: 1, borderColor: '#16a45f' }} />
+                                style={[styles.button, { marginRight: 5, marginLeft: 10, borderWidth: 1, borderColor: colorTheme }]} />
                             <Button
                                 onPress={() => {
                                     let selectedIds = [];
@@ -140,40 +180,46 @@ class Select2 extends Component {
                                     onSelect && onSelect(selectedIds);
                                     this.setState({ show: false, keyword: '', preSelectedItem: selectedItem });
                                 }}
-                                title='Chọn'
-                                backgroundColor={'#16a45f'}
-                                style={{ height: 36, width: 120, marginLeft: 5, marginRight: 10 }} />
+                                title={selectButtonText}
+                                backgroundColor={colorTheme}
+                                style={[styles.button, { marginLeft: 5, marginRight: 10 }]} />
                         </View>
-                    </View>
+                    </Animated.View>
                 </Modal>
                 {
                     preSelectedItem.length > 0
-                        ? <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-                            {
-                                preSelectedItem.map((tag, index) => {
-                                    return <TagItem
-                                        key={index}
-                                        onRemoveTag={() => {
-                                            let preSelectedItem = [];
-                                            let selectedIds = [];
-                                            let { data } = this.state;
-                                            data.map(item => {
-                                                if (item.id === tag.id) {
-                                                    item.checked = false;
-                                                }
-                                                if (item.checked) {
-                                                    preSelectedItem.push(item);
-                                                    selectedIds.push(item.id);
-                                                };
-                                            })
-                                            this.setState({ data, preSelectedItem });
-                                            onRemoveItem(selectedIds);
-                                        }}
-                                        tagName={tag.name} />
-                                })
-                            }
-                        </View>
-                        : <Text style={{ fontSize: 14, color: 'gray' }}>{title}</Text>
+                        ? (
+                            isSelectSingle
+                                ? <Text style={styles.selectedTitlte}>{preSelectedItem[0].name}</Text>
+                                : <View style={styles.tagWrapper}>
+                                    {
+                                        preSelectedItem.map((tag, index) => {
+                                            return (
+                                                <TagItem
+                                                    key={index}
+                                                    onRemoveTag={() => {
+                                                        let preSelectedItem = [];
+                                                        let selectedIds = [];
+                                                        let { data } = this.state;
+                                                        data.map(item => {
+                                                            if (item.id === tag.id) {
+                                                                item.checked = false;
+                                                            }
+                                                            if (item.checked) {
+                                                                preSelectedItem.push(item);
+                                                                selectedIds.push(item.id);
+                                                            };
+                                                        })
+                                                        this.setState({ data, preSelectedItem });
+                                                        onRemoveItem(selectedIds);
+                                                    }}
+                                                    tagName={tag.name} />
+                                            );
+                                        })
+                                    }
+                                </View>
+                        )
+                        : <Text style={styles.selectedTitlte}>{title}</Text>
                 }
             </TouchableOpacity>
         );
@@ -184,16 +230,64 @@ class Select2 extends Component {
 const styles = StyleSheet.create({
     container: {
         width: '100%', minHeight: 45, borderRadius: 2, paddingHorizontal: 16,
-        flexDirection: 'row', alignItems: 'center', borderWidth: 1, borderColor: '#cacaca',
-        paddingVertical: 4
+        flexDirection: 'row', alignItems: 'center', borderWidth: 1,
+        borderColor: '#cacaca', paddingVertical: 4
     },
     modalContainer: {
-        paddingTop: 16, backgroundColor: '#fff', borderRadius: 3,
-        maxHeight: height * 0.7,
+        paddingTop: 16, backgroundColor: '#fff', borderTopLeftRadius: 8, borderTopRightRadius: 8
     },
-    title: { color: '#16a45f', fontWeight: 'bold', fontSize: 16, marginBottom: 10, width: '100%', textAlign: 'center' },
-    line: { height: 1, width: '100%', backgroundColor: '#cacaca' },
+    title: {
+        fontWeight: 'bold', fontSize: 16, marginBottom: 16, width: '100%', textAlign: 'center'
+    },
+    line: {
+        height: 1, width: '100%', backgroundColor: '#cacaca'
+    },
+    inputKeyword: {
+        height: 40, borderRadius: 5, borderWidth: 1, borderColor: '#cacaca',
+        paddingLeft: 8, marginHorizontal: 24, marginTop: 16
+    },
+    buttonWrapper: {
+        marginVertical: 16, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'
+    },
+    button: {
+        height: 36, flex: 1
+    },
+    selectedTitlte: {
+        fontSize: 14, color: 'gray'
+    },
+    tagWrapper: {
+        flexDirection: 'row', flexWrap: 'wrap'
+    },
+    listOption: {
+        paddingHorizontal: 24,
+        paddingTop: 1
+    },
+    itemWrapper: {
+        borderBottomWidth: 1, borderBottomColor: '#eaeaea',
+        paddingVertical: 12, flexDirection: 'row', alignItems: 'center'
+    },
+    itemText: {
+        fontSize: 16, color: '#333', flex: 1
+    },
+    itemIcon: {
+        width: 30, textAlign: 'right'
+    },
+    empty: {
+        fontSize: 16, color: 'gray', alignSelf: 'center', textAlign: 'center', paddingTop: 16
+    }
 });
+
+Select2.propTypes = {
+    style: PropTypes.object,
+    title: PropTypes.string,
+    onSelect: PropTypes.func,
+    onRemoveItem: PropTypes.func,
+    popupTitle: PropTypes.string,
+    colorTheme: PropTypes.string,
+    isSelectSingle: PropTypes.bool,
+    cancelButtonText: PropTypes.string,
+    selectButtonText: PropTypes.string
+}
 
 //make this component available to the app
 export default Select2;
